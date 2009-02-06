1677ed7a513af6f4da73a5a1b332207f2d9238c6
John Prince <jtprince@gmail.com>
2009-02-05 17:09:50 -0700

    last couple of specs fixed

--- spec/ms/mascot/dat_spec.rb
+++ spec/ms/mascot/dat_spec.rb
@@ -2,13 +2,16 @@ require File.join(File.dirname(__FILE__), '../../tap_spec_helper.rb')
 require 'ms/mascot/dat'
 require 'ms/mascot/dat/query'
 
-@@file = '/home/jtprince/ms/data/090113_init/mini/F040565.dat'
 
 class DatUsageSpec < MiniTest::Spec
   include Ms::Mascot
 
+  before do
+    @file = Ms::TESTDATA + '/mascot/dat/F040565.dat'
+  end
+
   it 'gives direct access to queries' do
-    Dat.open(@@file) do |obj|
+    Dat.open(@file) do |obj|
       qrs = []  # for later spec
       obj.each_query do |query|
         query.must_be_kind_of Dat::Query
@@ -28,87 +31,35 @@ class DatUsageSpec < MiniTest::Spec
   end
 
   it 'lists section names' do
-    Dat.open(@@file) do |obj|
+    #p method_root
+    Dat.open(@file) do |obj|
       obj.section_names.must_equal ["parameters", "masses", "unimod", "enzyme", "header", "summary", "decoy_summary", "peptides", "decoy_peptides", "proteins", "query1", "query2", "query3", "query4", "index"]
     end
   end
 
   it 'returns sections' do
     # some of these are currently just Strings, but there they are.
-    Dat.open(@@file) do |obj|
+    Dat.open(@file) do |obj|
       %w(parameters masses unimod enzyme header summary decoy_summary peptides decoy_peptides proteins index).each do |meth|
         obj.section(meth).wont_be_nil
       end
     end
   end
 
-  #it 'just returns hashes for applicable sections' do
-    #Dat.open(@@file) do |obj|
-      #Dat::HASHES.each do |meth|
-        #hash = obj.send meth.to_sym
-        #hash.must_be_kind_of Hash
-        #hash.size.must_be :>=, 5
-      #end
-      ## just to make sure the content is there:
-      #obj.parameters['TOLU'].must_equal 'ppm'
-      #obj.header['date'].must_equal '1232579902'
-      #obj.masses['C'].must_equal '103.009185'
-      #obj.index['summary'].must_equal '495'
-    #end
-  #end
-#end
-
-#class DatTests < MiniTest::Spec
-
-#it 'indexes the file (with a byte index)' do
-#ind = File.open(@@file) do |io|
-#Dat.byte_index(io)
-#end
-
-    #first_last = {
-      #'parameters' => ["LICENSE=Licensed to: HHMI / University of Colorado, Chem and Biochem. (041U000579), (1 processor).", "INTERNALS=0.0,700.0"],
-      #'masses' => ['A=71.037114', 'NeutralLoss2_master=0.000000'],
-      #'index' => ['parameters=4', 'query4=1664'],
-      ## these are treated a little different
-      #1 => ['title=JP_PM3_0113_10ul_orb1%2e5369%2e5369%2e1%2edta', ["Ions1=121.060000", "19.2"]],
-      #4 => ['title=JP_PM3_0113_10ul_orb1%2e2149%2e2149%2e3%2edta', ["Ions1=251.200000", "4.1"]]
-
-    #}
-
-    #%w(parameters masses index) << [1,4].each do |key|
-      #ind_ar = 
-        #if key.is_a? Integer
-          #ind['query'][key]
-        #else
-          #ind[key]
-        #end
-
-      #string = IO.read(@@file, ind_ar.last, ind_ar.first)
-      #lines = string.split("\n")
-      #lines.first.must_equal first_last[key].first
-      #if key.is_a? Integer
-        ## test the first bit of the line and last bit of the line
-        #s_line = lines.last.split(':')
-        #s_line.first.must_equal first_last[key].last.first
-        #s_line.last.must_equal first_last[key].last.last
-      #else
-        #lines.last.must_equal first_last[key].last
-      #end
-
-    #end
-  #end
-
-  #it 'makes hashes from strings of parameters' do
-    #string = "charge=3+
-#mass_min=234.210000
-#mass_max=1984.020000
-#int_min=1.9
-#int_max=7689
-#num_vals=748
-#"
-    #exp = {"num_vals"=>"748", "int_max"=>"7689", "int_min"=>"1.9", "mass_max"=>"1984.020000", "mass_min"=>"234.210000", "charge"=>"3+"}
-    #Dat.str_to_hash(string).must_equal exp
-  #end
-
-
+  it 'returns hashes for applicable sections' do
+    Dat.open(@file) do |obj|
+      %w(parameters header masses index).each do |sec|
+        hash = obj.section(sec).data
+        hash.must_be_kind_of Hash
+        hash.size.must_be :>=, 5
+      end
+      # just to make sure the content is there:
+      obj.section('parameters').data['TOLU'].must_equal 'ppm'
+      obj.section('header').data['date'].must_equal '1232579902'
+      obj.section('masses').data['C'].must_equal '103.009185'
+      obj.section('index').data['summary'].must_equal '495'
+    end
+  end
 end
+
+
